6caa950e06eda566536753f9ec71b44b021adbf4
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-11-15 18:10:17 +0800

    Updated queries to match include resolve flags

--- php/database.php
+++ php/database.php
@@ -3,24 +3,26 @@
 
   mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
   $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+  $resolve_value = 0;
+
   if($conn->connect_error) { die('Connection failed' . $conn->connect_error); }
   if(DEBUG_MODE) { echo 'CONNECTED TO DATABASE!<br>'; }
 
   function insert_new_feedback($name_first, $name_last, $email, $subject, $message)
   {
-    global $conn;
-    $sql = 'INSERT INTO `feedbacks` (`id`, `name_first`, `name_last`, `email`, `subject`, `message`, `post_time`) VALUES (NULL, ?, ?, ?, ?, ?, current_timestamp())';
+    global $conn, $resolve_value;
+    $sql = 'INSERT INTO `feedbacks` (`id`, `resolved`, `name_first`, `name_last`, `email`, `subject`, `message`, `post_time`) VALUES (NULL, ?, ?, ?, ?, ?, ?, current_timestamp())';
     $preppedStmt = $conn->prepare($sql);
     if(!$preppedStmt) { return false; }
-    return $preppedStmt->bind_param('sssss', $name_first, $name_last, $email, $subject, $message) ? $preppedStmt->execute() : false;
+    return $preppedStmt->bind_param('isssss', $resolve_value, $name_first, $name_last, $email, $subject, $message) ? $preppedStmt->execute() : false;
   }
 
   function insert_new_inquiry($package_id, $name_first, $name_last, $email, $subject, $message)
   {
-    global $conn;
-    $sql = 'INSERT INTO `package_inquiries` (`id`, `package_id`, `name_first`, `name_last`, `email`, `subject`, `message`, `post_time`) VALUES (NULL, ?, ?, ?, ?, ?, ?, current_timestamp())';
+    global $conn, $resolve_value;
+    $sql = 'INSERT INTO `package_inquiries` (`id`, `resolved`, `package_id`, `name_first`, `name_last`, `email`, `subject`, `message`, `post_time`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, current_timestamp())';
     $preppedStmt = $conn->prepare($sql);
     if(!$preppedStmt) { return false; }
-    return $preppedStmt->bind_param('isssss', $package_id, $name_first, $name_last, $email, $subject, $message) ? $preppedStmt->execute() : false;
+    return $preppedStmt->bind_param('iisssss', $resolve_value, $package_id, $name_first, $name_last, $email, $subject, $message) ? $preppedStmt->execute() : false;
   }
 ?>
