9a9434c4c86e285b3f0b2aadba9dc7c0bf090b37
Johnnyvision <jspepenancheta52402@gmail.com>
2022-11-16 22:46:17 +0800

    Reverted login/logout php.

--- php/logout.php
+++ php/logout.php
@@ -1,32 +1,5 @@
-<html><head>
-<script type="text/javascript">
-function logout() {
-    var hexagon;
-    if (window.XMLHttpRequest) {
-        hexagon = new XMLHttpRequest();
-    }
-    // code for IE
-    else if (window.ActiveXObject) {
-        hexagon=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    if (window.ActiveXObject) {
-      // IE clear HTTP Authentication
-      document.execCommand("ClearAuthenticationCache");
-      window.location.href='admin.php';
-    } else {
-        hexagon.open("GET", 'admin.php', true, "logout", "logout");
-        hexagon.send("");
-        hexagon.onreadystatechange = function() {
-            if (hexagon.readyState == 4) {window.location.href='admin.php';}
-        }
-
-
-    }
-    return false;
-}
-</script>
-</head>
-<body>
-<a href="#" onclick="logout();">Log out</a>
-</body>
-</html>
+<?php
+    session_start();
+    session_destroy();
+    header("Location: login.php");
+?>
