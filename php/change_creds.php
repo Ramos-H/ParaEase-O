cf23a9eb0110b9943ed1242921506eafc591192f
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-11-19 22:44:00 +0800

    Reorganized change_creds logic

--- php/change_creds.php
+++ php/change_creds.php
@@ -1,84 +1,67 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Change Credentials</title>
-</head>
-<body>
+  <head>
+    <title>Change Credentials Test</title>
+  </head>
+  <body>
+    <h2>Current credentials</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+      Old Username: <input type="text" name="oldName"><br><br>
+      Old Password: <input type="text" name="oldPass"><br><br>
+      New Username: <input type="text" name="newName"><br><br>
+      New Password: <input type="text" name="newPass"><br><br>
+      Confirm Password: <input type="text" name="newPassTwo"><br><br>
+      <input type="submit" name="submit" value="Submit">
+    </form>
 
-<h2>Current credentials</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Old Username: <input type="text" name="oldName">
-  
-  <br><br>
-  Old Password: <input type="text" name="oldPass">
-  
-  <br><br>
-  New Username: <input type="text" name="newName">
-  
-  <br><br>
-  New Password: <input type="text" name="newPass">
-  
-  <br><br>
-  Confirm Password: <input type="text" name="newPassTwo">
-  
-  <br><br>
+    <?php
+      require_once 'utils.php';
+      require_once 'database.php';
+      require_once 'constants.php';
 
-  <input type="submit" name="submit" value="Submit">  
+      if(isset($_POST['submit'])) // when user submit POST request
+      {
+        $errors = errCheck(); // check if errors exist
+        if(!empty($errors['oldNameErr'])) { phpAlert($errors['oldNameErr']); };
+        if(!empty($errors['oldPassErr'])) { phpAlert($errors['oldPassErr']); };
+        if(!empty($errors['newNameErr'])) { phpAlert($errors['newNameErr']); };
+        if(!empty($errors['newPassErr'])) { phpAlert($errors['newPassErr']); };
+        if(!empty($errors['confirmErr'])) { phpAlert($errors['confirmErr']); };
+      }
 
-</form>
+      function errCheck() // validate user input. Throw errors into errors array
+      { 
+        //Define the array of error within this function 
+        $errors = array('oldNameErr' => '', 'oldPassErr' => '', 'newNameErr' => '' , 'newPassErr' => '' , 'confirm' => ''); // array containing errors
 
+        $username         = isset($_POST['oldName'])    ? trim($_POST['oldName'])    : null;
+        $password         = isset($_POST['oldPass'])    ? trim($_POST['oldPass'])    : null;
+        $new_username     = isset($_POST['newName'])    ? trim($_POST['newName'])    : null;
+        $new_password     = isset($_POST['newPass'])    ? trim($_POST['newPass'])    : null;
+        $confirm_password = isset($_POST['newPassTwo']) ? trim($_POST['newPassTwo']) : null;
 
-<?php
-$errors = array('oldNameErr' => '', 'oldPassErr' => '', 'newNameErr' => '' , 'newPassErr' => '' , 'confirm' => '');
+        if(check_str_empty($username)) { $errors['oldNameErr'] = 'Username cannot be empty'; } // old name req *
+        if(check_str_empty($password)) { $errors['oldPassErr'] = 'Password cannot be empty'; } // old pass req *
+        
+        if(strlen($new_username) > 30) { $errors['newNameErr'] = 'Error! New username too long'; } // more than 30 chars not allowed *
+        
+        if(!check_str_empty($new_password))
+        {
+          if(strlen($new_password) < 8) { $errors['newPassErr'] = 'Error! New password must be at least 8 characters'; } // min 8 chars req *
 
-if(isset($_POST['submit'])){  // when user submit POST request
+          if(check_str_empty($confirm_password)) { $errors['confirmErr'] = 'Confirm cannot be empty'; } //  confirm pass req if new pass is existing *
+          elseif($new_password !== $confirm_password) { $errors['confirmErr'] = 'New password and confirm password does not match'; } // new password and confirm must match *
+        }
 
-    $errors = errCheck();
-    if(!empty($errors['oldNameErr'])) {phpAlert($errors['oldNameErr']);}; // check if errors exist
-    if(!empty($errors['oldPassErr'])) {phpAlert($errors['oldPassErr']);};
-    if(!empty($errors['newNameErr'])) {phpAlert($errors['newNameErr']);};
-    if(!empty($errors['newPassErr'])) {phpAlert($errors['newPassErr']);};
-    if(!empty($errors['confirm'])) {phpAlert($errors['confirm']);};
-}
+        //Return the errors
+        return $errors;
+      }
 
-function errCheck() {               // validate user input. Throw errors into errors array
-    //Define the array of error within this function 
-    $errors = array('oldNameErr' => '', 'oldPassErr' => '', 'newNameErr' => '' , 'newPassErr' => '' , 'confirm' => ''); // array containing errors
-
-    if(empty($_POST['oldName'])) {
-        $errors['oldNameErr'] = 'Username cannot be empty'; // old name req *
-    }
-
-    if(empty($_POST['oldPass'])) {
-        $errors['oldPassErr'] = 'Password cannot be empty'; // old pass req *
-    }
-
-    if(strlen($_POST['newName']) > 30) {
-        $errors['oldPassErr'] = 'Error! Username too long'; // more than 30 chars not allowed *
-    }
-
-    if(strlen($_POST['newPass']) < 8) { 
-        $errors['newPassErr'] = 'Error! Password too long'; // min 8 chars allowed *
-    }
-    
-    if(empty($_POST['newName']) && !empty($_POST['newPass'])) {
-        $errors['newPassErr'] = 'New Username is required before entering a new password'; //  cannot enter new password if new username is empty
-    }
-
-    if(!empty($_POST['newPass']) && empty($_POST['newPassTwo'])) {
-        $errors['confirm'] = 'Confirm cannot be empty'; //  confirm pass req if new pass is existing *
-    }
-
-    if ($_POST["newPass"] !== $_POST["newPassTwo"]) { 
-        $errors['confirm'] = 'New Password and Confirm does not match'; // new password and confirm match *
-    }
-    //Return the errors
-    return $errors;
-}
-
-function phpAlert($msg){    // in case errors exist
-    echo $msg;
-    echo '<script type="text/javascript">alert("' . $msg . '")</script>';
-};
-?>
-</body>
+      function phpAlert($msg) // In case errors exist
+      {
+        echo $msg;
+        echo '<script type="text/javascript">alert("' . $msg . '")</script>';
+      };
+    ?>
+  </body>
+</html>
