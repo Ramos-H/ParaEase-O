34d5d5df8b05e3d0bec61f63dbf2a2d31c684b11
Kupirikopiko <113969897+Kupirikopiko@users.noreply.github.com>
2022-11-20 13:55:30 +0800

    Add files via upload

---
+++ ICS2608-Project-main/ICS2608-Project-main/assets/js/aboutUs.js
@@ -0,0 +1,10 @@
+function sendMessageBtn(){
+    var firstName = document.getElementsByName("firstNameInput").value;
+    var lastName = document.getElementsByName("lastNameInput").value;
+    var emailAddress = document.getElementsByName("emailAddInput").value; 
+    var subject = document.getElementsByName("subjectInput").value;
+    var message = document.getElementsByName("messageInput").value;
+
+}
+
+
