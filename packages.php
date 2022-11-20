7c44b8c1b4fa8e56797d15e49b68ef594ee86dab
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-11-20 22:20:30 +0800

    Reorganized project folder structure

---
+++ packages.php
@@ -0,0 +1,112 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!--Feeling Passionate Font-->
+    <link href="http://fonts.cdnfonts.com/css/feeling-passionate" rel="stylesheet">
+    <!--Poppins Font-->
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <!--Page title Icon-->
+    <link rel="shortcut icon" href="assets\img\icon.png" type="image/x-icon">
+    <!--Main CSS-->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <title>ParaEase'O</title>
+
+</head>
+<body class="body-packages">
+        <nav class="navbar navbar-expand-lg fixed-top px-5">
+            <a href="#"><img class= "navbar-brand" src="assets/img/ParaEase'O_Brand.png" alt="ParaEase'O Brand"></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item pe-5">
+                  <a class="nav-link" href="index.html">HOME</a>
+                </li>
+                
+                <li class="nav-item active dropdown pe-5">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    EXPLORE
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="">GO-TO PLACES</a></li>
+                    <li><a class="dropdown-item" href="">GO-TO ACTIVITIES</a></li>
+                    <li><a class="dropdown-item" href="#">TRAVEL PACKAGES</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#.html">ABOUT US</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+      <header class="masthead-packages">
+          <div class="container-fluid text-center d-flex justify-content-center">
+          
+            <div class="row justify-content-center ">
+                <div>
+                </div>
+                <div class="col-4 box card py-5 mx-5 my-2" style="width: 12rem; height: auto;">
+                    <div class=" card-body py-5 align-items-center">
+                        <h2>Tour A</h2>
+                        <h1>₱ 1,200</h1>
+                        <p>per pax</p>
+                        <a href="#">
+                        <button type="button" class="btn-lg btn select-btn px-3 py-1">SELECT PACKAGE</button>
+                        </a>                
+                    </div>
+                </div>
+              
+                <div class="col-4 box card py-5 mx-5 my-2" style="width: 12rem; height: auto;">
+                    <div class="card-body py-5 align-items-center">
+                        <h2>Tour B</h2>
+                        <h1>₱ 1,300</h1>
+                        <p>per pax</p>
+                        <a href="#">
+                        <button type="button" class="btn-lg btn select-btn px-3 py-1">SELECT PACKAGE</button>
+                     </a>                
+                    </div>
+                </div>
+    
+                <div class="col-4 box card py-5 mx-5 my-2" style="width: 12rem; height: auto;">
+                    <div class="card-body py-5 align-items-center">
+                        <h2>Tour C</h2>
+                        <h1>₱ 1,400</h1>
+                        <p>per pax</p>
+                        <a href="#">
+                        <button type="button" class="btn-lg btn select-btn px-3 py-1">SELECT PACKAGE</button>
+                        </a>                
+                    </div>
+                </div>
+            </div>
+
+          </div>
+        </div>
+      </header>
+      
+      <section class="map-section" id="about">
+        <div class="container-fluid text-center">
+            <img class="map my-5" src="assets/img/map.jpg" alt="Map">
+
+        </div>
+      
+    </section>
+  <!--Bootstrap JS bundle-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
+
+<!--  <div class="row">
+          <div class="col-6 border">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241609.576289215!2d119.45416559033447!3d11.192972260553368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33b65515ef5c9e0f%3A0xfc4e665b599b0455!2sEl%20Nido%2C%20Palawan!5e0!3m2!1sen!2sph!4v1668441207367!5m2!1sen!2sph" width="680" height="680" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div class="col-6 text-center pt-5">
+            <h1 class=""> Welcome to El Nido!</h1>
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse repellendus est, debitis cumque dicta maiores non minima aspernatur pariatur, maxime quam, ut sit nisi reprehenderit fuga. At veritatis nisi magnam? lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi cupiditate, possimus dicta illo veniam quis unde eos adipisci perspiciatis, et architecto quas eius expedita excepturi porro, officiis accusamus consequuntur consequatur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia veniam necessitatibus sit deleniti aliquid consequuntur suscipit dolore natus nesciunt voluptates, expedita itaque eveniet quia nemo? Distinctio excepturi ratione voluptatem repudiandae?</p>
+            </div>
+          </div> -->
