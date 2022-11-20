6b1a630738453b931d75e4eb7b18edd7c193b2c6
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-11-20 11:19:52 +0800

    Renamed array variables in change_creds

--- php/change_creds.php
+++ php/change_creds.php
@@ -6,11 +6,11 @@
   <body>
     <h2>Current credentials</h2>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-      Old Username: <input type="text" name="oldName"><br><br>
-      Old Password: <input type="text" name="oldPass"><br><br>
-      New Username: <input type="text" name="newName"><br><br>
-      New Password: <input type="text" name="newPass"><br><br>
-      Confirm Password: <input type="text" name="newPassTwo"><br><br>
+      Old Username: <input type="text" name="username"><br><br>
+      Old Password: <input type="text" name="password"><br><br>
+      New Username: <input type="text" name="new_username"><br><br>
+      New Password: <input type="text" name="new_password"><br><br>
+      Confirm Password: <input type="text" name="confirm_password"><br><br>
       <input type="submit" name="submit" value="Submit">
     </form>
 
@@ -21,11 +21,11 @@
 
       if(isset($_POST['submit'])) // when user submit POST request
       {
-        $username         = isset($_POST['oldName'])    ? trim($_POST['oldName'])    : null;
-        $password         = isset($_POST['oldPass'])    ? trim($_POST['oldPass'])    : null;
-        $new_username     = isset($_POST['newName'])    ? trim($_POST['newName'])    : null;
-        $new_password     = isset($_POST['newPass'])    ? trim($_POST['newPass'])    : null;
-        $confirm_password = isset($_POST['newPassTwo']) ? trim($_POST['newPassTwo']) : null;
+        $username         = isset($_POST['username'])         ? trim($_POST['username'])         : null;
+        $password         = isset($_POST['password'])         ? trim($_POST['password'])         : null;
+        $new_username     = isset($_POST['new_username'])     ? trim($_POST['new_username'])     : null;
+        $new_password     = isset($_POST['new_password'])     ? trim($_POST['new_password'])     : null;
+        $confirm_password = isset($_POST['confirm_password']) ? trim($_POST['confirm_password']) : null;
 
         $has_username         = !check_str_empty($username);
         $has_password         = !check_str_empty($password);
@@ -45,23 +45,23 @@
           echo sprintf('Has confirm password: %s<br>', boolToStr($has_confirm_password));
         }
 
-        $errors = array('oldNameErr' => '', 
-                        'oldPassErr' => '', 
-                        'newNameErr' => '' , 
-                        'newPassErr' => '' , 
-                        'confirmErr' => ''); // array containing errors
+        $errors = array('username' => '', 
+                        'password' => '', 
+                        'new_username' => '' , 
+                        'new_password' => '' , 
+                        'confirm_password' => ''); // array containing errors
 
-        if(!$has_username) { $errors['oldNameErr'] = 'Username cannot be empty'; } // old name req *
-        if(!$has_password) { $errors['oldPassErr'] = 'Password cannot be empty'; } // old pass req *
+        if(!$has_username) { $errors['username'] = 'Username cannot be empty'; } // old name req *
+        if(!$has_password) { $errors['password'] = 'Password cannot be empty'; } // old pass req *
         
-        if($new_username_too_long) { $errors['newNameErr'] = 'Error! New username too long'; } // more than 30 chars not allowed *
+        if($new_username_too_long) { $errors['new_username'] = 'Error! New username too long'; } // more than 30 chars not allowed *
         
         if($has_new_password)
         {
-          if($new_password_too_short) { $errors['newPassErr'] = 'Error! New password must be at least 8 characters'; } // min 8 chars req *
+          if($new_password_too_short) { $errors['new_password'] = 'Error! New password must be at least 8 characters'; } // min 8 chars req *
 
-          if(!$has_confirm_password) { $errors['confirmErr'] = 'Confirm cannot be empty'; } //  confirm pass req if new pass is existing *
-          elseif($new_password !== $confirm_password) { $errors['confirmErr'] = 'New password and confirm password does not match'; } // new password and confirm must match *
+          if(!$has_confirm_password) { $errors['confirm_password'] = 'Confirm cannot be empty'; } //  confirm pass req if new pass is existing *
+          elseif($new_password !== $confirm_password) { $errors['confirm_password'] = 'New password and confirm password does not match'; } // new password and confirm must match *
         }
 
         if($has_username && $has_password)
@@ -79,11 +79,11 @@
           }
         }
 
-        if(!check_str_empty($errors['oldNameErr'])) { echo $errors['oldNameErr'] . '<br>'; };
-        if(!check_str_empty($errors['oldPassErr'])) { echo $errors['oldPassErr'] . '<br>'; };
-        if(!check_str_empty($errors['newNameErr'])) { echo $errors['newNameErr'] . '<br>'; };
-        if(!check_str_empty($errors['newPassErr'])) { echo $errors['newPassErr'] . '<br>'; };
-        if(!check_str_empty($errors['confirmErr'])) { echo $errors['confirmErr'] . '<br>'; };
+        if(!check_str_empty($errors['username']))         { echo sprintf('%s<br>', $errors['username']); }
+        if(!check_str_empty($errors['password']))         { echo sprintf('%s<br>', $errors['password']); }
+        if(!check_str_empty($errors['new_username']))     { echo sprintf('%s<br>', $errors['new_username']); }
+        if(!check_str_empty($errors['new_password']))     { echo sprintf('%s<br>', $errors['new_password']); }
+        if(!check_str_empty($errors['confirm_password'])) { echo sprintf('%s<br>', $errors['confirm_password']); }
       }
     ?>
   </body>
