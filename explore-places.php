273841732570dc15d6c197e3f8fae64416aa07a5
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-11-21 20:28:43 +0800

    Reorganized structures of explore places and activities

--- explore-places.php
+++ explore-places.php
@@ -1,5 +1,5 @@
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,87 +11,83 @@
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <!--Page title Icon-->
     <link rel="shortcut icon" href="assets/icon.jpg" type="image/x-icon">
-
     <!--Main CSS-->
     <link rel="stylesheet" href="styles/main.css">
-
     <title>ParaEase'O | Your Go-To Places</title>
+  </head>
 
-</head>
-<body class="body-places">
-  <?php include 'inc/header.php'?>
-    
-  <main>
+  <body class="body-places">
+    <?php include 'inc/header.php'?>
+    <main>
       <h1 class="h1-places"> Your go to places!</h1>
       <h6 class="h6-places">FEATURED EL NIDO TOURIST SPOTS</h6>
       <div class="container">
         <div class="row">
           <div class="col text-center">
-          <img class="img-places" src="assets/big-lagoon.jpg">
-        </div>
-        
-        <div class="col">
-          <h4 class="h4-places text-lg-left">Big Lagoon</h4>
-          <p class="p-places text-lg-left">The Big Lagoon, located on one of El Nido's islands, is one of the best places to kayak and admire the rock formations and forest density. Paddling into the entrance channel and seeing the crystal clear blue water with fine white sand is only a taste of the Big Lagoon, which is almost dark blue or green in color and cool to the touch making it ideal for the upcoming hot seasons.</p>
-        </div>
-
-        <div class="row">
-          <div class="col text-center order-lg-2">
-            <img class="img-places" src="assets/small-lagoon.jpg">
+            <img class="img-places" src="assets/big-lagoon.jpg">
           </div>
 
-          <div class="col order-lg-1">
-            <h4 class="h4-places text-lg-end">Small Lagoon</h4>
-            <p class="p-places text-lg-end">The Small Lagoon, neighboring the Big Lagoon, is filled with light green almost turquoise in color cool water with unbelievable rock formations all around. Naturally formed in an area full of greenery, the Small Lagoon is a spot to cool off and enjoy the relaxing pool of water with no distractions.</p>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col text-center">
-          <img class="img-places" src="assets/snake-island.jpg">
-        </div>
-        
-        <div class="col">
-          <h4 class="h4-places text-lg-left">Snake Island</h4>
-          <p class="p-places text-lg-left">Located southern of the island of El Nido, the Snake Island, named after its white sandy beach spanning across like a silhouette of a snake and officially named as Vigan Beach, is a great spot for swimming or basking in the crystal clear turquoise waters but it is the only island that will give you the experience and feeling of walking in the middle of the sea.</p>
-        </div>
-
-        <div class="row">
-          <div class="col text-center order-lg-2">
-            <img class="img-places" src="assets/cudugnon-cave.jpg">
+          <div class="col">
+            <h4 class="h4-places text-lg-left">Big Lagoon</h4>
+            <p class="p-places text-lg-left">The Big Lagoon, located on one of El Nido's islands, is one of the best places to kayak and admire the rock formations and forest density. Paddling into the entrance channel and seeing the crystal clear blue water with fine white sand is only a taste of the Big Lagoon, which is almost dark blue or green in color and cool to the touch making it ideal for the upcoming hot seasons.</p>
           </div>
 
-          <div class="col order-lg-1">
-            <h4 class="h4-places text-lg-end">Cudugnon Cave</h4>
-            <p class="p-places text-lg-end">Tucked away at the west part of Lagen Island of El Nido, Cudugnon Cave sits at the foot of Mount Maateg, a scenic view that welcomes tourists on their arrival to the beautiful island. Best enjoyed at low-tide, the Cudugnon Cave features an archeological exploration for adventure seekers and tourists alike.</p>
-          </div>
-        </div>
+          <div class="row">
+            <div class="col text-center order-lg-2">
+              <img class="img-places" src="assets/small-lagoon.jpg">
+            </div>
 
-        <div class="row">
-          <div class="col text-center">
-          <img class="img-places" src="assets/hidden-beach.jpg">
-        </div>
-        
-        <div class="col">
-          <h4 class="h4-places text-lg-left">Hidden Beach</h4>
-          <p class="p-places text-lg-left">Concealed with meteorite-shaped rocks lies the Hidden beach filled with extraordinary corals and fine white sand. A perfect place to unwind and listen to the sound of crashing waves while exploring the side of a cliff filled with nature and discover sea life hidden in every nook and cranny of the rock formations that surround the beach.</p>
-        </div>
-
-        <div class="row">
-          <div class="col text-center order-lg-2">
-            <img class="img-places" src="assets/secret-beach.jpg">
+            <div class="col order-lg-1">
+              <h4 class="h4-places text-lg-end">Small Lagoon</h4>
+              <p class="p-places text-lg-end">The Small Lagoon, neighboring the Big Lagoon, is filled with light green almost turquoise in color cool water with unbelievable rock formations all around. Naturally formed in an area full of greenery, the Small Lagoon is a spot to cool off and enjoy the relaxing pool of water with no distractions.</p>
+            </div>
           </div>
 
-          <div class="col order-lg-1">
-            <h4 class="h4-places text-lg-end">Secret Beach</h4>
-            <p class="p-places text-lg-end">Tucked away in the corner of Matinloc Island, a hidden gem named the Secret Beach is shining even under the shade created when the sun kisses the cliffs. A swim under a slit in the island holds a beach filled with numerous fish, beautiful rock formations and corals visible at a small distance from the surface.</p>
+          <div class="row">
+            <div class="col text-center">
+              <img class="img-places" src="assets/snake-island.jpg">
+            </div>
+
+            <div class="col">
+              <h4 class="h4-places text-lg-left">Snake Island</h4>
+              <p class="p-places text-lg-left">Located southern of the island of El Nido, the Snake Island, named after its white sandy beach spanning across like a silhouette of a snake and officially named as Vigan Beach, is a great spot for swimming or basking in the crystal clear turquoise waters but it is the only island that will give you the experience and feeling of walking in the middle of the sea.</p>
+            </div>
+
+            <div class="row">
+              <div class="col text-center order-lg-2">
+                <img class="img-places" src="assets/cudugnon-cave.jpg">
+              </div>
+
+              <div class="col order-lg-1">
+                <h4 class="h4-places text-lg-end">Cudugnon Cave</h4>
+                <p class="p-places text-lg-end">Tucked away at the west part of Lagen Island of El Nido, Cudugnon Cave sits at the foot of Mount Maateg, a scenic view that welcomes tourists on their arrival to the beautiful island. Best enjoyed at low-tide, the Cudugnon Cave features an archeological exploration for adventure seekers and tourists alike.</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col text-center">
+                <img class="img-places" src="assets/hidden-beach.jpg">
+              </div>
+
+              <div class="col">
+                <h4 class="h4-places text-lg-left">Hidden Beach</h4>
+                <p class="p-places text-lg-left">Concealed with meteorite-shaped rocks lies the Hidden beach filled with extraordinary corals and fine white sand. A perfect place to unwind and listen to the sound of crashing waves while exploring the side of a cliff filled with nature and discover sea life hidden in every nook and cranny of the rock formations that surround the beach.</p>
+              </div>
+
+              <div class="row">
+                <div class="col text-center order-lg-2">
+                  <img class="img-places" src="assets/secret-beach.jpg">
+                </div>
+
+                <div class="col order-lg-1">
+                  <h4 class="h4-places text-lg-end">Secret Beach</h4>
+                  <p class="p-places text-lg-end">Tucked away in the corner of Matinloc Island, a hidden gem named the Secret Beach is shining even under the shade created when the sun kisses the cliffs. A swim under a slit in the island holds a beach filled with numerous fish, beautiful rock formations and corals visible at a small distance from the surface.</p>
+                </div>
+            </div>
           </div>
         </div>
-
-        </div>
-      
-  </main>
-    
+      </div>
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-</body>
+  </body>
 </html>
