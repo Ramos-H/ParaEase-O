6f1858f42f829e940cbd102c61a0b99b398e2a40
Hans Ramos <97885787+Ramos-H@users.noreply.github.com>
2022-11-26 00:32:02 +0800

    Removed unused util functions

--- php/utils.php
+++ php/utils.php
@@ -4,19 +4,6 @@ function boolToStr($value)
   return $value ? 'true' : 'false';
 }
 
-function print_err_text_on_error($concerned_property, $has_submitted, $has_no_input, $input_too_long)
-{
-  $invalid_input_template = '<span class="invalid-input">%s</span><br>';
-  $no_input_message_template = 'Please enter your %s.';
-  $long_input_message_template = 'The %s you entered is too long.';
-
-  if($has_submitted)
-  {
-    if($has_no_input) { echo sprintf($invalid_input_template, sprintf($no_input_message_template, $concerned_property)); }
-    elseif ($input_too_long) { echo sprintf($invalid_input_template, sprintf($long_input_message_template, $concerned_property)); }
-  }
-}
-
 function time_elapsed_string($datetime, $level = 7) {
   $now = new DateTime;
   $ago = new DateTime($datetime);
